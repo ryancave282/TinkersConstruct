6ffddd4b4a69911889451818d26ac6bf5a01a35b
KnightMiner <knightminer4@gmail.com>
2024-09-15 17:56:42 -0400

    Add the tool background to the tool building category
    Not technically using the right icon, I use the render tool instead of the layout icon as I was too lazy to add a way to locate it. Its difficult when we need to create a custom slot array instead of using the layout directly

--- src/main/java/slimeknights/tconstruct/plugin/jei/ToolBuildingCategory.java
+++ src/main/java/slimeknights/tconstruct/plugin/jei/ToolBuildingCategory.java
@@ -1,5 +1,6 @@
 package slimeknights.tconstruct.plugin.jei;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.Getter;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
@@ -10,6 +11,7 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
@@ -37,7 +39,8 @@ public class ToolBuildingCategory implements IRecipeCategory<ToolBuildingRecipe>
   private final IDrawable icon;
   @Getter
   private final IDrawable background;
-  private final IDrawable anvil, slot;
+  private final IDrawable anvil, slotBg, slotBorder;
+  private final IDrawable itemCover;
   private static final int WIDTH = 134;
   private static final int HEIGHT = 66;
   private static final int ITEM_SIZE = 16;
@@ -45,8 +48,10 @@ public class ToolBuildingCategory implements IRecipeCategory<ToolBuildingRecipe>
   public ToolBuildingCategory(IGuiHelper guiHelper) {
     this.icon = guiHelper.createDrawableItemStack(TinkerTools.pickaxe.get().getRenderTool());
     this.background = guiHelper.createDrawable(BACKGROUND_LOC, 122, 77, WIDTH, HEIGHT);
-    this.slot = guiHelper.createDrawable(BACKGROUND_LOC, 24, 14, SLOT_SIZE, SLOT_SIZE);
+    this.slotBg = guiHelper.createDrawable(BACKGROUND_LOC, 144, 59, SLOT_SIZE, SLOT_SIZE);
+    this.slotBorder = guiHelper.createDrawable(BACKGROUND_LOC, 162, 59, SLOT_SIZE, SLOT_SIZE);
     this.anvil = guiHelper.createDrawable(BACKGROUND_LOC, 128, 61, ITEM_SIZE, ITEM_SIZE);
+    this.itemCover = guiHelper.createDrawable(BACKGROUND_LOC, 122, 77, 70, 60);
   }
 
   @Override
@@ -77,13 +82,41 @@ public class ToolBuildingCategory implements IRecipeCategory<ToolBuildingRecipe>
 
   @Override
   public void draw(ToolBuildingRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
-    if (recipe.requiresAnvil()) {
-      this.anvil.draw(stack, 76, 44);
-    }
+    // first, draw the item background
+    ItemStack outputStack = recipe.getOutput() instanceof IModifiableDisplay modifiable ? modifiable.getRenderTool() : recipe.getOutput().asItem().getDefaultInstance();
+    PoseStack renderPose = RenderSystem.getModelViewStack();
+    renderPose.pushPose();
+    renderPose.mulPoseMatrix(stack.last().pose());
+    renderPose.translate(5, 6.5, 0);
+    renderPose.scale(3.7f, 3.7f, 1.0f);
+    Minecraft.getInstance().getItemRenderer().renderGuiItem(outputStack, 0, 0);
+    renderPose.popPose();
 
+    // next, overlay the item with transparent grey, makes it appear transparent
+    RenderSystem.enableBlend();
+    RenderSystem.disableDepthTest();
+    RenderSystem.applyModelViewMatrix();
+    RenderSystem.setShaderColor(1, 1, 1, 0.82f);
+    itemCover.draw(stack, 5, 6);
+
+    // next, draw slot backgrounds very transparent
+    RenderSystem.setShaderColor(1, 1, 1, 0.28f);
     for (LayoutSlot layoutSlot : recipe.getLayoutSlots()) {
       // need to offset by 1 because the inventory slot icons are 18x18
-      this.slot.draw(stack, layoutSlot.getX() + X_OFFSET - 1, layoutSlot.getY() + Y_OFFSET - 1);
+      this.slotBg.draw(stack, layoutSlot.getX() + X_OFFSET - 1, layoutSlot.getY() + Y_OFFSET - 1);
+    }
+    // finally, draw slot borders opaque
+    RenderSystem.setShaderColor(1, 1, 1, 1);
+    for (LayoutSlot layoutSlot : recipe.getLayoutSlots()) {
+      // need to offset by 1 because the inventory slot icons are 18x18
+      this.slotBorder.draw(stack, layoutSlot.getX() + X_OFFSET - 1, layoutSlot.getY() + Y_OFFSET - 1);
+    }
+    RenderSystem.disableBlend();
+    RenderSystem.enableDepthTest();
+
+    // draw anvil icon if anvil is required
+    if (recipe.requiresAnvil()) {
+      this.anvil.draw(stack, 76, 44);
     }
   }
 
